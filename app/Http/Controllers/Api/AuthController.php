d3cdd5b7d44f3297d14d952ac34d67d129ce88c4
5llo <mhmdkhalil822@gmail.com>
2023-09-05 08:45:26 +0300

    the last version

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Traits\GeneralTrait;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use App\Models\Code;
+use App\Models\Collage;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
+
+use Illuminate\Database\Schema\Builder;
+
+
+
+
+class AuthController extends Controller
+{
+    use GeneralTrait;
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'bail|required|unique:users|max:10',
+            'number' => 'bail|required|unique:users|min:10|max:10',
+            'uuid' => 'bail|required|exists:collages'
+ ]);
+        if ($validator->fails()) {
+            return $this->errorResponse($validator->errors(), 422);
+        }
+        try {
+
+                        $collage_id = Collage::where('uuid', $request['uuid'])->first()->id;
+                        $code = (string)random_int(100000, 999999);
+
+                        $user = User::create([
+                            'name' => $request->name,
+                            'number' => $request->number,
+                            'collage_id' => $collage_id,
+                            'code' => $code
+
+                        ]);
+                        return $this->successResponse([], 'Registered Successfully.');
+            } catch (\Exception $ex) {
+            return $this->errorResponse($ex->getMessage(), 500);
+        }
+    }
+
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' =>[ 'required','exists:users','max:12'],
+            'code' =>[ 'required','max:6']
+        ]);
+
+
+        if ($validator->fails()) {
+
+            return $this->errorResponse($validator->errors(), 422);
+        }
+
+
+            $user=User::where('name',$request->name)->first();
+
+            if($user->code == $request->code ){
+             $collage = $user->collage;
+
+            $data['name'] = $collage->name;
+            $data['specialization'] = $collage->name;
+            $data['collage'] = $collage->type;
+            $data['url'] = str_replace('"', '', asset('users/' . $user->url));
+            $data['token'] = $user->createToken('MyApp')->plainTextToken;
+
+          return $this->successResponse($data, 'User has logged in successfully.');
+        }else {
+            return $this->errorResponse('The name or code uncorrect', 400);
+        }
+    }
+
+
+    public function logout(Request $request)
+    {
+        auth('sanctum')->user()->tokens()->delete();
+
+        return $this->successResponse([], 'User has logged out successfully.');
+    }
+}
+
+
+
+
+// $user = DB::table('users')   example one
+//                 ->latest()
+//                 ->first();
+
+
+//  $user = User::orderBy('id', 'DESC')->first(); example two
+
+
+// $user = User::get()->latest();            example three
---
+++ app/Http/Controllers/profileController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Traits\GeneralTrait;
+use App\Models\Image;
+use App\Models\User;
+use Illuminate\Support\Facades\File;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\Rule;
+
+class profileController extends Controller
+{
+    use GeneralTrait;
+    public function update_profile(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', Rule::unique('users')->ignore(Auth::id())],
+            'number' => ['required', Rule::unique('users')->ignore(Auth::id()), 'min:10', 'max:10'],
+
+
+        ]);
+
+
+        if ($validator->fails()) {
+            return $this->errorResponse($validator->errors(), 422);
+        }
+        $filename = null;
+        $user = Auth::user();
+        if ($request->hasfile('url')) {
+
+            $image_name = $user->url;
+            $image_path = public_path('users/' . $image_name);
+            if (File::exists($image_path)) {
+                File::delete($image_path);
+            }
+
+            $file = $request->file('url');
+            $extension = $file->getClientOriginalExtension();
+            $filename = time() . '.' . $extension;
+            $file->move('users/', $filename);
+        }
+        try {
+
+
+
+            $user->name = $request->name;
+            $user->number = $request->number;
+            $user->url = $filename;
+            $user->save();
+            $user = Auth::user();
+
+            $data['name'] = $user->name;
+            $data['number'] = $user->number;
+            $data['specialization'] = $user->collage->name;
+            $data['collage'] = $user->collage->type;
+            $data['url'] = str_replace('"', '', asset('users/' . $user->url));
+
+            return $this->successResponse($data, 'The changes have updated successfully.');
+        } catch (\Exception $ex) {
+            return $this->errorResponse($ex->getMessage(), 500);
+        }
+
+        return response()->json(['response' => ['code' => '200', 'message' => 'تم تعديل البيانات بنجاح',]]);
+    }
+}
